4b357e24529d9391e411b7aa147cb9406a8b035a
Alice Tchang <alice.tchang@insee.fr>
2021-06-24 18:20:57 +0200

    mp_name no longer mandatory

--- R/replace_series.R
+++ R/replace_series.R
@@ -1,30 +1,38 @@
-
-#' Function to automate the replacement of selected series and their metadata by the same series from another workspace
-
-
-#' This function allows to update a selection of series. It is the partial version of the update_metadata function,
-#' of the rjdworkspace package.
-
+#' Partial update of a workspace metadata
+#'
+#' \code{replace_series()} allows to update a selection of series by the same-named series 
+#' from another workspace. When only the metadata differs, it is the partial version of the update_metadata function.
+#'
+#' 
 #' @param ws1 The workspace to update
 #' @param ws2 The workspace containing the most up-to-date version of the selected_series series
-#' @param mp_name The name of the multiprocessing containing the series to update
 #' @param selected_series The vector containing the series-to-update's names.
-#' @param print_indications A boolean to print indications on the processing status
-
+#' @param mp_name The name of the multiprocessing containing the series to update (optional)
+#' @param print_indications A boolean to print indications on the processing status (optional)
+#' 
+#' @details If the argument \code{mp_name} is unspecified, the update will be performed using 
+#' the workspaces' first SAProcessing.
+#'
 #' @rdname replace_series
 #' @return the updated \code{workspace}
 #' @examples \dontrun{replace_series(ws1, ws2, "SAProcessing-1", c("serie1", "serie2"), TRUE)}
 #' @export
 #'
-replace_series <- function(ws1, ws2, mp_name, selected_series, print_indications=FALSE){
+replace_series <- function(ws1, ws2, selected_series, mp_name=NA, print_indications=FALSE){
   
   # Verification of the parameters type
   if(!inherits(ws1, "workspace")){stop("The first argument must be a workspace")}
   if(!inherits(ws2, "workspace")){stop("The second argument must be a workspace")}
   if(is.null(selected_series)){stop("The selected_series list is empty!")}
-    else {
-      if(!is.character(selected_series)){stop("The selected_series list must contain characters (the series names).")}
-      }
+  else {
+    if(!is.character(selected_series)){stop("The selected_series list must contain characters (the series names).")}
+  }
+  
+  # if "mp_name" isn't specified but "print_indications" is
+  # ie. if mp_name is TRUE or FALSE
+  if(!is.na(mp_name)&inherits(mp_name,"logical")){
+    print_indications <- mp_name 
+    mp_name <- NA}
   
   # Check that the workspaces aren't empty
   if(is.null(count(ws1))){
@@ -50,11 +58,22 @@ replace_series <- function(ws1, ws2, mp_name, selected_series, print_indications
   
   # Verification that both workspaces contain the SAP to be updated
   # and storage of its position in each workspace
-  pos_mp1 <- which(names_saps1 == mp_name)
-  pos_mp2 <- which(names_saps2 == mp_name)
+  
+  # if no SAP has been declared, the first of each workspaces will be used
+  pos_mp1 <- 1  
+  pos_mp2 <- 1
+  
+  
+  # otherwise, we retrieve the SAP's position in each workspace 
+  if(!is.na(mp_name)){
+    pos_mp1 <- which(names_saps1 == mp_name)
+    pos_mp2 <- which(names_saps2 == mp_name)
+  }
+  
+  if(print_indications){print(paste0("pos_mp1=",pos_mp1))}
   
   # End of the program if the SAP can't be found in one of the workspaces
-  if(is.null(pos_mp1)){
+  if(is.null(pos_mp1)||is.na(pos_mp1)){
     print("The chosen SAP couldn't be found in the first workspace.")
     return(FALSE)
   }
@@ -121,7 +140,9 @@ replace_series <- function(ws1, ws2, mp_name, selected_series, print_indications
       if (print_indications) {print("ok")}
     }
     
-    print(paste0("Series updating done for the SAP ", mp_name,"."))
+    if(is.na(mp_name)){print("Update done for the first workspaces' SAProcessing.")}
+    else{print(paste0("Series updating done for the SAP ", mp_name,"."))
+    }
     
     return(ws1)
     
@@ -129,16 +150,22 @@ replace_series <- function(ws1, ws2, mp_name, selected_series, print_indications
 }
 
 
-#' Auxiliary functions
-
+#' Auxiliary functions 
+#' 
 #' Generic function to identify and return the duplicates in a list
-
+#' 
+#' @details
+#' \code{verif_duplicates()} identifies and returns the duplicates in a list 
+#' \code{verif_ws_duplicates()} identifies duplicated series in a SAProcessing (SAP) 
+#' and SAProcessings in a workspace
+#' 
 #' @param s a list of characters
 #' 
 #' @rdname replace_series
 #' @return If there are no duplicates, the function returns an empty data frame. 
 #' Otherwise, it returns a data frame giving the name and number of duplicates found within the argument (list).
-#' @examples \dontrun{
+#' @examples 
+#' \dontrun{
 #' s <- c("a","b","a","c","a","c")
 #'result <- verif_duplicates(s)
 #'result
@@ -152,11 +179,8 @@ verif_duplicates <- function(s){
   res2 <- res[res$Freq>1,c("name","Freq")]
   return(res2)  
 }
-
-
-
-#' Function to identify duplicated series in a SAProcessing (SAP) or of SAProcessings in a workspace
-
+#'
+#'
 #' @param ws The workspace to scan
 #' @param print_indications A boolean to print indications on the processing status
 #' @rdname replace_series
